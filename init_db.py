7036b1b5bcb9df2d5ecd84f3f2cd033cd5a6894d
semimaks <semimaks@mail.ru>
2021-09-03 00:47:53 +0300

    Подправлен код

--- init_db.py
+++ init_db.py
@@ -2,8 +2,9 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
+
 with open('schema.sql') as f:
-    connection.executescript(f.read()) # executescript позволят выполнить несколько операторов SQL одновременно
+    connection.executescript(f.read())
 
 cur = connection.cursor()
 
